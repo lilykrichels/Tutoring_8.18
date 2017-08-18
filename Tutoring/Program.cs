f52feefc062913916fce175c2d09fe12de3ec06f
lilykrichels <lily.krichels@gmail.com>
2017-08-18 19:08:00 -0400

    added practice problems

--- Tutoring/Program.cs
+++ Tutoring/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿  using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +10,21 @@ namespace Tutoring
     {
         static void Main(string[] args)
         {
+            float salad = 5.12f;
+            float totalNow = 27.10f;
+            Console.WriteLine(totalNow + salad);
+
+            float monty = 16.72f;
+            float larry = 4.12f;
+            Console.WriteLine(larry + monty);
+
+
+            int middleNum = (8640/ 3);
+            int smallNum = (middleNum - 1);
+            int largeNum = (middleNum + 1);
+            Console.WriteLine(smallNum);
+            Console.WriteLine(middleNum);
+            Console.WriteLine(largeNum);
         }
     }
 }
